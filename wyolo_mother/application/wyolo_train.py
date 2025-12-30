c8612a8f97f7152d13e8f646f46df518e645f561
wisrovi rodriguez <wisrovi.rodriguez@gmail.com>
2025-12-30 15:28:48 +0100

    [FEATURE] Worker test 30/12/2025

---
+++ wyolo_mother/application/wyolo_train.py
@@ -0,0 +1,45 @@
+# sys.path.insert(0, os.path.join(os.path.dirname(__file__), "lib", "src"))
+import tempfile
+import yaml
+from setproctitle import setproctitle
+from application.utils.util import get_complete_config, get_user_config
+
+# from lib.src.wyolo.core.yolo_train import create_trainer, train
+from lib.src.wyolo.trainer.trainer_wrapper import create_trainer, train
+
+
+setproctitle("wyolo_service")
+
+
+def main():
+    # python main.py "/app/lib/datasets_config_examples/clasification/config_train.yaml"
+    # python main.py "/wyolo/control_server/datasets/detection/Deteksi komponen elektronik.v1i.yolov8/config_train.yaml"
+    # python main.py "/wyolo/control_server/datasets/segmentation/ArchitecturePlan/config_train.yaml"
+    user_config_train = get_user_config("/app/lib/datasets_config_examples/clasification/config_train.yaml")
+
+    config_dict, config_path = get_complete_config(user_config=user_config_train)
+    # trial_number = final_config.get("trial_number", 0)
+
+    with tempfile.TemporaryDirectory(prefix="wyolo_") as temp_dir:
+        config_dict["tempfile"] = temp_dir
+
+        with open(config_path, "w") as f:
+            yaml.dump(config_dict, f)
+
+        trainer, request_config = create_trainer(
+            config_path=config_path, trial_number=1
+        )
+        if "train" in request_config:
+            fitness = config_dict.get("sweeper", {}).get("fitness", "fitness")
+            request_config = train(trainer, request_config, fitness)
+
+    print(request_config)
+
+
+if __name__ == "__main__":
+    """
+    EXAMPLE:
+        python yolo_train.py --config_path="/datasets/clasificacion/clasificador_arepo_perfil/config_train.yaml" --trial_number=1
+    """
+
+    main()
